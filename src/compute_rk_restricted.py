22b84e6528d2a058b6fc2c086f74b10999c1a322
Vineet Kumar <vineeku6@in.ibm.com>
2017-11-17 11:35:57 +0530

    Add Rk numbers

--- src/compute_rk_restricted.py
+++ src/compute_rk_restricted.py
@@ -1,7 +1,8 @@
 import logging, argparse
 
-#Candidates to check
-RK = [1, 2, 5]
+R1 = 0
+R2 = 1
+R5 = 4
 
 def setup_args():
   parser = argparse.ArgumentParser()
@@ -19,7 +20,7 @@ def find_rank_index(index, candidates):
       return rank
 
 
-def process_max_rank(candidates, max_rank, datum_num):
+def process_candidates(candidates, max_rank, datum_num):
   if len(candidates) == 0:
     return
 
@@ -36,7 +37,7 @@ def main():
   logging.info(args)
 
   #Set top-rank to k for each datum
-  max_rank = [args.k for datum in range(args.num_datum)]
+  rank_gt = [args.k for datum in range(args.num_datum)]
 
   last_datum_num = -1
   candidates = []
@@ -46,8 +47,8 @@ def main():
 
     if datum_num != last_datum_num:
       if last_datum_num >= 0:
-        process_max_rank(candidates, max_rank, last_datum_num)
-        logging.info('Datum: %d Rank_0: %d/%d'%(last_datum_num, max_rank[last_datum_num], len(candidates)))
+        process_candidates(candidates, rank_gt, last_datum_num)
+        logging.info('Datum: %d Rank_0: %d/%d'%(last_datum_num, rank_gt[last_datum_num], len(candidates)))
       last_datum_num = datum_num
       candidates = []
 
@@ -55,8 +56,37 @@ def main():
     score = float(scores_line.strip())
     candidates.append((score, index))
 
-  process_max_rank(candidates, max_rank, last_datum_num)
-  logging.info('Datum: %d Rank_0: %d/%d' % (last_datum_num, max_rank[last_datum_num], len(candidates)))
+  process_candidates(candidates, rank_gt, last_datum_num)
+  logging.info('Datum: %d Rank_0: %d/%d' % (last_datum_num, rank_gt[last_datum_num], len(candidates)))
+
+  num_r1 = 0.0
+  num_r2 = 0.0
+  num_r5 = 0.0
+
+  gt_present = 0
+  gt_absent = 0
+  total = 0.0 + len(rank_gt)
+  #Given Rank of GT for each datum, it is trivial to compute R@k numbers
+  for datum in rank_gt:
+    if datum == args.k:
+      gt_absent += 1
+    else:
+      gt_present += 1
+
+    if datum == R1:
+      num_r1 += 1
+      num_r2 += 1
+      num_r5 += 1
+    elif datum <= R2:
+      num_r2 += 1
+      num_r5 += 1
+    elif datum <= R5:
+      num_r5 += 1
+
+
+  logging.info('R1:%d R2:%d R@5:%d GT(%d/%d)' % (int(num_r1), int(num_r2), int(num_r5), gt_present, gt_absent))
+  logging.info('R@1:%.3f R@2:%.3f R@5:%.3f'%((num_r1/total), (num_r2/total), (num_r5/total)))
+
 
 if __name__ == '__main__':
   logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
