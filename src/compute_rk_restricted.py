7b776b01e63b08a2b44c71729c13ba96f9fe678b
Vineet Kumar <vineeku6@in.ibm.com>
2017-11-17 12:06:01 +0530

    Sort by scores

--- src/compute_rk_restricted.py
+++ src/compute_rk_restricted.py
@@ -16,18 +16,20 @@ def setup_args():
 
 def find_rank_index(index, candidates):
   for rank, candidate in enumerate(candidates):
-    if candidate[1] == 0:
+    if candidate[1] == index:
       return rank
 
 
 def process_candidates(candidates, max_rank, datum_num):
+  #Sorry, no candidates!
   if len(candidates) == 0:
     return
 
+  #GT is not present in candidates, set rank_gt=k
   if candidates[0][1] != 0:
     return
 
-  candidates = sorted(candidates, key=lambda candidate: candidate[1], reverse=True)
+  candidates = sorted(candidates, key=lambda candidate: candidate[0], reverse=True)
   rank_0 = find_rank_index(0, candidates)
   max_rank[datum_num] = rank_0
 
@@ -48,7 +50,10 @@ def main():
     if datum_num != last_datum_num:
       if last_datum_num >= 0:
         process_candidates(candidates, rank_gt, last_datum_num)
-        logging.info('Datum: %d Rank_0: %d/%d'%(last_datum_num, rank_gt[last_datum_num], len(candidates)))
+        if rank_gt[last_datum_num] == args.k:
+          logging.info('Datum: %d GT absent C: %d'%(datum_num, len(candidates)))
+        else:
+          logging.info('Datum: %d Rank_0: %d/%d'%(last_datum_num, rank_gt[last_datum_num], len(candidates)-1))
       last_datum_num = datum_num
       candidates = []
 
@@ -86,7 +91,6 @@ def main():
 
   logging.info('R1:%d R2:%d R@5:%d GT(%d/%d)' % (int(num_r1), int(num_r2), int(num_r5), gt_present, gt_absent))
   logging.info('R@1:%.3f R@2:%.3f R@5:%.3f'%((num_r1/total), (num_r2/total), (num_r5/total)))
-
 
 if __name__ == '__main__':
   logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
