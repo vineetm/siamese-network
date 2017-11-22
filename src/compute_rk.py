df0f5173637cebf8f2f8d65a99ead67c27669912
Vineet Kumar <vineeku6@in.ibm.com>
2017-11-22 20:04:39 +0530

    4 decimal places

--- src/compute_rk.py
+++ src/compute_rk.py
@@ -1,4 +1,5 @@
 import logging, argparse
+import numpy as np
 
 R1 = 0
 R2 = 1
@@ -33,32 +34,38 @@ def process_candidates(candidates, max_rank, datum_num):
   rank_0 = find_rank_index(0, candidates)
   max_rank[datum_num] = rank_0
 
-
 def main():
   args = setup_args()
   logging.info(args)
 
-  #Set top-rank to k for each datum
+  #We want to compute Rank of index=0 (GT)
+  #By default this is set to Max_Candidates+1
   rank_gt = [args.k for datum in range(args.num_datum)]
+  mrr = [0.0 for _ in range(args.num_datum)]
 
+  #These two are used to compute compression we can obtain
   total_datums = 0.0
   total_candidates = 0.0
 
-  last_datum_num = -1
+  last_datum_num = 0
   candidates = []
+
+  #Map contains two numbers datum and index of candidate in original file..
   for scores_line, map_line in zip(open(args.scores_file), open(args.map_file)):
     datum_num, index = map_line.split(',')
     datum_num = int(datum_num)
 
+    #Datum index changed, we can thus now process all candidates gathered so far
     if datum_num != last_datum_num:
-      if last_datum_num >= 0:
-        process_candidates(candidates, rank_gt, last_datum_num)
-        total_candidates += len(candidates)
-        total_datums += 1
-        if rank_gt[last_datum_num] == args.k:
-          logging.info('Datum: %d GT absent C: %d'%(last_datum_num, len(candidates)))
-        else:
-          logging.info('Datum: %d Rank_0: %d/%d'%(last_datum_num, rank_gt[last_datum_num], len(candidates)-1))
+      total_candidates += len(candidates)
+      total_datums += 1
+
+      process_candidates(candidates, rank_gt, last_datum_num)
+      if rank_gt[last_datum_num] == args.k:
+        logging.info('Datum: %d GT absent C: %d'%(last_datum_num, len(candidates)))
+      else:
+        mrr[last_datum_num] = 1.0 / (rank_gt[last_datum_num] + 1)
+        logging.info('Datum: %d Rank_0: %d/%d'%(last_datum_num, rank_gt[last_datum_num], len(candidates)-1))
       last_datum_num = datum_num
       candidates = []
 
@@ -66,8 +73,9 @@ def main():
     score = float(scores_line.strip())
     candidates.append((score, index))
 
+  #This for the last set
   process_candidates(candidates, rank_gt, last_datum_num)
-  logging.info('Datum: %d Rank_0: %d/%d' % (last_datum_num, rank_gt[last_datum_num], len(candidates)))
+  logging.info('Datum: %d Rank_0: %d/%d' % (last_datum_num, rank_gt[last_datum_num], len(candidates)-1))
 
   num_r1 = 0.0
   num_r2 = 0.0
@@ -93,10 +101,9 @@ def main():
     elif datum <= R5:
       num_r5 += 1
 
-
   logging.info('R1:%d R2:%d R@5:%d GT(%d/%d)' % (int(num_r1), int(num_r2), int(num_r5), gt_present, gt_absent))
-  logging.info('R@1:%.3f R@2:%.3f R@5:%.3f Avg:.3f%'%((num_r1/total), (num_r2/total), (num_r5/total),
-                                                      (total_candidates/total_datums)))
+  logging.info('R@1:%.4f R@2:%.4f R@5:%.4f Avg:%.4f MRR: %.4f'%((num_r1/total), (num_r2/total), (num_r5/total),
+                                                      (total_candidates/total_datums), np.average(mrr)))
 
 if __name__ == '__main__':
   logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
