a26fc087f20e3894961b59bf181dfca85030e930
Vineet Kumar <vineeku6@in.ibm.com>
2017-12-04 23:03:20 +0530

    Fix prep data

--- prep-data/prep_data.py
+++ prep-data/prep_data.py
@@ -3,7 +3,7 @@ Separate data into three parallel files: txt1, txt2 and labels
 Apllied to train, valid, and test
 '''
 import logging, argparse, csv
-
+import numpy as np
 POS_LABEL = 1
 NEG_LABEL = 0
 
@@ -14,12 +14,13 @@ NUM_COLS_TRAIN = 3
 def setup_args():
   parser = argparse.ArgumentParser()
   parser.add_argument('-train', default=False, action='store_true')
-  parser.add_argument('-valid', default=False, action='store_true', help='Valid data has ')
-  parser.add_argument('-preserve_case', default=False, action='if set, we preserve case of text')
+  parser.add_argument('-valid', default=False, action='store_true')
+  parser.add_argument('-preserve_case', default=False, action='store_true')
   parser.add_argument('-input_csv', default=None)
   parser.add_argument('-out_txt1', default=None)
   parser.add_argument('-out_txt2', default=None)
   parser.add_argument('-out_labels', default=None)
+  parser.add_argument('-seed', default=1543, type=int)
   args = parser.parse_args()
   return args
 
@@ -33,7 +34,7 @@ def process_training_data(input_csv, out_txt1, out_txt2, out_labels):
   with open(input_csv) as fr:
     reader = csv.reader(fr)
     #Skip header
-    reader.next()
+    next(reader, None)
 
     for row in reader:
       assert len(row) == NUM_COLS_TRAIN
@@ -42,18 +43,35 @@ def process_training_data(input_csv, out_txt1, out_txt2, out_labels):
       fw_labels.write('%s\n'%row[2].lower().strip())
       num_rows_written += 1
 
-      if num_rows_written % 10000 == 0:
+      if num_rows_written % 100000 == 0:
         logging.info('Input: %s IP Wrote %d rows'%(input_csv, num_rows_written))
 
   logging.info('Input: %s Done Wrote %d rows'%(input_csv, num_rows_written))
+
+
+def process_valid_data(input_csv, out_txt1, out_txt2, out_labels, num_neg=-1):
+  fw_txt1 = open(out_txt1, 'w')
+  fw_txt2 = open(out_txt2, 'w')
+  fw_labels = open(out_labels, 'w')
+
+  num_rows_written = 0
+  with open(input_csv) as fr:
+    reader = csv.reader(fr)
+    # Skip header
+    next(reader, None)
+
+    for row in reader:
+
 
 
 def main():
   args = setup_args()
   logging.info(args)
 
+  np.random.seed(args.seed)
   if args.train:
     process_training_data(args.input_csv, args.out_txt1, args.out_txt2, args.out_labels)
+  elif args.valid:
 
 
 
--- prep-data/prep_data.sh
+++ prep-data/prep_data.sh
@@ -8,4 +8,4 @@ TRAIN_TXT1="${OUT_DIR}/train.txt1"
 TRAIN_TXT2="${OUT_DIR}/train.txt2"
 TRAIN_LABELS="${OUT_DIR}/train.labels"
 
-python prep_data.py -train -input_csv $TRAIN_CSV -out_txt1 $TRAIN_TXT1 -out_txt2 $TRAIN_TXT2 -out_labels $TRAIN_TXT2 
+python prep_data.py -train -input_csv $TRAIN_CSV -out_txt1 $TRAIN_TXT1 -out_txt2 $TRAIN_TXT2 -out_labels $TRAIN_LABELS 
