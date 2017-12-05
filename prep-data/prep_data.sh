afd345f71909f8be7770fe5543f57683bbe483dd
Vineet Kumar <vineeku6@in.ibm.com>
2017-12-05 14:27:35 +0530

    Code to create a vocab file

--- prep-data/prep_data.sh
+++ prep-data/prep_data.sh
@@ -10,6 +10,7 @@ TRAIN_TXT1="${OUT_DIR}/train.txt1"
 TRAIN_TXT2="${OUT_DIR}/train.txt2"
 TRAIN_LABELS="${OUT_DIR}/train.labels"
 python prep_data.py -train -input_csv $TRAIN_CSV -out_txt1 $TRAIN_TXT1 -out_txt2 $TRAIN_TXT2 -out_labels $TRAIN_LABELS 
+echo "prep train done"
 
 #Prepare validation data for siamese
 VALID_CSV="${DATA_DIR}/valid.csv"
@@ -17,3 +18,13 @@ SIAMESE_VALID_TXT1="${OUT_DIR}/svalid.txt1"
 SIAMESE_VALID_TXT2="${OUT_DIR}/svalid.txt2"
 SIAMESE_VALID_LABELS="${OUT_DIR}/svalid.labels"
 python prep_data.py -siamese_valid -input_csv $VALID_CSV -out_txt1 $SIAMESE_VALID_TXT1 -out_txt2 $SIAMESE_VALID_TXT2 -out_labels $SIAMESE_VALID_LABELS 
+echo "prep svalid done"
+
+#Prepare vocabulary. Restrict to words that appear atleast 50 times. First word is UNK
+VOCAB="${OUT_DIR}/vocab.txt"
+ALL_VOCAB="${OUT_DIR}/all.vocab.txt"
+MIN_FREQ=50
+
+python create_vocab.py -txt1 $TRAIN_TXT1 -txt2 $TRAIN_TXT2 -min_freq $MIN_FREQ -vocab $VOCAB -all_vocab $ALL_VOCAB
+echo "prep vocab done"
+
