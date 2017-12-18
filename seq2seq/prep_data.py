17f162a34b348fc713521ed27771d07b54d9b51f
Vineet Kumar <vineeku6@in.ibm.com>
2017-12-18 17:39:41 +0530

    Replace words with cluster representative

--- seq2seq/prep_data.py
+++ seq2seq/prep_data.py
@@ -1,11 +1,19 @@
 import argparse, logging
 
+EOU = '__eou__'
+EOT = '__eot__'
+
 
 def setup_args():
   parser = argparse.ArgumentParser()
   parser.add_argument('-stopw', help='stopwords file')
   parser.add_argument('-word_clusters', help='Word clusters')
-
+  parser.add_argument('-txt1')
+  parser.add_argument('-txt2')
+  parser.add_argument('-labels', default=None)
+  parser.add_argument('-out_txt1')
+  parser.add_argument('-out_txt2')
+  parser.add_argument('-out_index')
   args = parser.parse_args()
   return args
 
@@ -32,6 +40,29 @@ def read_stopwords(file_name):
   return stop_words
 
 
+def get_cluster_repr(sentence, word2cluster, clusters):
+  cluster_ints = [word2cluster[word] for word in sentence.split() if word in word2cluster]
+  sorted_clusters = sorted(list(set(cluster_ints)))
+  cluster_words = [clusters[cluster][0] for cluster in sorted_clusters]
+  return cluster_words
+
+
+def process_target(sentence, word2cluster, clusters):
+  cluster_words = get_cluster_repr(sentence, word2cluster, clusters)
+  return cluster_words
+
+
+def process_source(sentence, word2cluster, clusters):
+  final_words = []
+  for turn in sentence.split(EOT)[:-1]:
+    for utt in turn.split(EOU)[:-1]:
+      words = [clusters[word2cluster[word]][0] if word in word2cluster else word for word in utt.split()]
+      final_words.extend(words)
+      final_words.append(EOU)
+    final_words.append(EOT)
+  return final_words
+
+
 def main():
   args = setup_args()
   logging.info(args)
@@ -41,6 +72,28 @@ def main():
 
   word2cluster, clusters = build_cluster_map(args.word_clusters)
   logging.info('#Clusters: %d Word assigned:%d'%(len(clusters), len(word2cluster)))
+
+  fw_txt1 = open(args.out_txt1, 'w')
+  fw_txt2 = open(args.out_txt2, 'w')
+  fw_index = open(args.out_index, 'w')
+
+  index = 0
+  for txt1, txt2, label in zip(open(args.txt1), open(args.txt2), open(args.labels)):
+    label = int(label)
+    if label == 0:
+      index += 1
+      continue
+
+    txt2_clusters = process_target(txt2, word2cluster, clusters)
+    if len(txt2_clusters) == 0:
+      index += 1
+      continue
+
+    txt1_words = process_source(txt1, word2cluster, clusters)
+    fw_txt1.write('%s\n'%' '.join(txt1_words))
+    fw_txt2.write('%s\n'%' '.join(txt2_clusters))
+    fw_index.write('%d\n'%index)
+    index += 1
 
 
 if __name__ == '__main__':
--- seq2seq/prep_data.sh
+++ seq2seq/prep_data.sh
@@ -3,4 +3,13 @@ DATA_DIR=$1
 STOPW="${DATA_DIR}/stopw_word2vec.txt"
 WORD_CLUSTERS="${DATA_DIR}/word_clusters.out"
 
-python prep_data.py -stopw $STOPW -word_clusters $WORD_CLUSTERS
+TRAIN_TXT1="${DATA_DIR}/train.txt1"
+TRAIN_TXT2="${DATA_DIR}/train.txt2"
+TRAIN_LABELS="${DATA_DIR}/train.labels"
+
+OUT_TRAIN_TXT1="${DATA_DIR}/train.seq2seq.txt1"
+OUT_TRAIN_TXT2="${DATA_DIR}/train.seq2seq.txt2"
+OUT_TRAIN_INDEX="${DATA_DIR}/train.seq2seq.index"
+
+
+python prep_data.py -stopw $STOPW -word_clusters $WORD_CLUSTERS -txt1 $TRAIN_TXT1 -txt2 $TRAIN_TXT2 -labels $TRAIN_LABELS -out_txt1 $OUT_TRAIN_TXT1 -out_txt2 $OUT_TRAIN_TXT2 -out_index $OUT_TRAIN_INDEX
