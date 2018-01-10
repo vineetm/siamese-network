2691d82a0b81dcdee667bc5cb7adc2a98bb5ada8
Vineet Kumar <vineeku6@in.ibm.com>
2018-01-10 11:22:14 +0530

    remove dead code"

--- eval/select_best_candidate.py
+++ eval/select_best_candidate.py
@@ -1,31 +1,41 @@
 import argparse, logging
 import numpy as np
+import os
 
 
 def setup_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-candidates')
-    parser.add_argument('-scores')
-    parser.add_argument('-best_candidate')
+    parser.add_argument('-candidates_dir')
+    parser.add_argument('-scores_dir')
+    parser.add_argument('-best_candidate_suffix')
+    parser.add_argument('-num', default=10, type=int)
     args = parser.parse_args()
     return args
+
+
+def select_best_candidate(candidates_f, scores_f):
+    candidates = []
+    scores = []
+
+    for candidate, score in zip(open(candidates_f), open(scores_f)):
+        candidates.append(candidate)
+        scores.append(float(score.strip()))
+    max_score_index = np.argmax(scores)
+    del scores
+    return candidates[max_score_index]
 
 
 def main():
     args = setup_args()
     logging.info(args)
 
-    candidates = []
-    scores = []
+    for k in range(args.num+1):
+        candidates_f = os.path.join(args.candidates_dir, f'{k}.txt2')
+        scores_f = os.path.join(args.scores_dir, f'{k}.scores')
+        best_candidate = select_best_candidate(candidates_f, scores_f)
 
-    for candidate, score in zip(open(args.candidates), open(args.scores)):
-        candidates.append(candidate)
-        scores.append(float(score.strip()))
-
-    logging.info(f'Num candidates: {len(candidates)}')
-    max_score_index = np.argmax(scores)
-    with open(args.best_candidate, 'w') as fw:
-        fw.write(candidates[max_score_index])
+        with open(os.path.join(args.scores_dir, f'{k}.{args.best_candidate_suffix}'), 'w') as fw:
+            fw.write(best_candidate)
 
 
 if __name__ == '__main__':
