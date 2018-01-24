fa30cd05b0962b893f750803cf7e105b3f54170f
Vineet Kumar <vineeku6@in.ibm.com>
2018-01-24 15:45:52 +0530

    Fix avg

--- eval/compute_rk_embedding.py
+++ eval/compute_rk_embedding.py
@@ -50,7 +50,7 @@ def main():
 
     # Find number of predictions
     cmd = f'wc -l < {args.predictions}'
-    num_predictions = subprocess.getoutput(cmd)
+    num_predictions = int(subprocess.getoutput(cmd))
     logging.info(f'Predictions: {num_predictions}')
 
     #Load Word2vec
@@ -60,12 +60,20 @@ def main():
     fr_candidates = open(args.candidates)
 
     num_r1, num_r2, num_r5 = 0., 0., 0.
+    num_no_embedding = 0
+    mrr = []
     for index, prediction in enumerate(open(args.predictions)):
         prediction_vector = get_embedding_average_score(prediction, w2v)
+        candidates = get_candidates(fr_candidates, args.num)
+
+        if prediction_vector is None:
+            num_no_embedding += 1
+            mrr.append(0.0)
+            continue
 
         #Now find vectors for 10 candidates
         candidate_vectors = [get_embedding_average_score(candidate, w2v)
-                            for candidate in get_candidates(fr_candidates, args.num)]
+                            for candidate in candidates]
 
         candidate_scores = [compute_distance(prediction_vector, candidate_vec) for candidate_vec in candidate_vectors]
         assert len(candidate_scores) == args.num
@@ -75,18 +83,22 @@ def main():
 
         if rank_0 == 1:
             num_r1 += 1.0
-        elif rank_0 <= 2:
-            num_r1 += 1.0
-            num_r2 += 1.0
-        elif rank_0 <= 5:
-            num_r1 += 1.0
             num_r2 += 1.0
             num_r5 += 1.0
-        if index % 10:
-            logging.info(f'NR1: {num_r1} NR2: {num_r2} NR5: {num_r5} / {index}')
+        elif rank_0 == 2:
+            num_r2 += 1.0
+            num_r5 += 1.0
+        elif rank_0 <= 5:
+            num_r5 += 1.0
+
+        mrr.append(1.0 / rank_0)
+        if index % 10 == 0:
+            logging.info(f'NR1: {num_r1} NR2: {num_r2} NR5: {num_r5}  MRR: {np.average(mrr)}/ {index} '
+                         f'No embedding: {num_no_embedding}')
 
     logging.info(f'NR1: {num_r1} NR2: {num_r2} NR5: {num_r5} / {num_predictions}')
-    logging.info(f'R1: {num_r1/num_predictions} R2: {num_r2/num_predictions} R5: {num_r5/num_predictions}')
+    logging.info(f'R1: {num_r1/num_predictions} R2: {num_r2/num_predictions} R5: {num_r5/num_predictions} '
+                 f'MRR: {np.average(mrr)} NE: {num_no_embedding}')
 
 
 if __name__ == '__main__':
